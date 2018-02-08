e8e58927cd04884c9baee957f7fdd6f4a40d3f11
Andrew Steinheiser <me@iamandrew.io>
2018-02-08 00:57:42 -0700

    adding react native svg

--- android/app/src/main/java/com/khor/MainApplication.java
+++ android/app/src/main/java/com/khor/MainApplication.java
@@ -3,6 +3,7 @@ package com.khor;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
             new SplashScreenReactPackage(),
             new VectorIconsPackage(),
             new RCTCameraPackage()
